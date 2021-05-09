e5dfb5db37ccdc3335f089e4208e1f833395869c
xinhaohe245 <xh245@cornell.edu>
2021-05-09 18:27:34 -0400

    debugged most routes, working on friend request routes

--- hack_challenge/app.py
+++ hack_challenge/app.py
@@ -7,27 +7,23 @@ from flask import Flask
 from flask import request
 
 app = Flask(__name__)
-db_filename = "cms.db"
+db_filename = "letsdoit.db"
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_ECHO"] = True
+app.config["SQLALCHEMY_ECHO"] = False
 
 db.init_app(app)
 with app.app_context():
     db.create_all()
 
-
 def success_response(data, code=200):
     return json.dumps({"success": True, "data": data}), code
-
 
 def failure_response(message, code=404):
     return json.dumps({"success": False, "error": message}), code
 
-
-# 注册 需要跳转到登录界面
-@app.route("/api/register/")
+@app.route("/api/register/", methods=["POST"])
 def register():
     body = json.loads(request.data.decode())
     name = body.get('name')
@@ -38,87 +34,84 @@ def register():
         return failure_response("no password entered")
     salt = os.urandom(32)
     password = salt + hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
-    uid = ''.join(random.sample(string.digits, 8))
-    possible_user = User.query.filter_by(uid=uid).first()
-    while possible_user is not None:
-        uid = ''.join(random.sample(string.digits, 8))
-        possible_user = User.query.filter_by(uid=uid).first()
-    new_user = User(name=name, password=password, uid=uid, public_lists=[], private_lists=[], sharing_lists=[],
-                    friends=[])
+    # uid = ''.join(random.sample(string.digits, 8))
+    # possible_user = User.query.filter_by(uid=uid).first()
+    # while possible_user is not None:
+    #     uid = ''.join(random.sample(string.digits, 8))
+    #     possible_user = User.query.filter_by(uid=uid).first()
+    # new_user = User(name=name, password=password, uid=uid, public_lists=[], private_lists=[], sharing_lists=[],
+    #                 friends=[])
+    new_user = User(name=name, password=password)
     db.session.add(new_user)
     db.session.commit()
-    return success_response("successfully registered!")
+    return success_response(new_user.serialize(), 201)
 
-
-
-# 登录
 @app.route("/api/login/", methods=['POST'])
 def login():
     body = json.loads(request.data.decode())
-    uid = body.get('uid')
-    if uid is None:
-        return failure_response("no uid entered")
+    name = body.get('name')
+    if name is None:
+        return failure_response("no name entered")
     password = body.get('password')
     if password is None:
         return failure_response("no password entered")
-    user = User.query.filter_by(uid=uid).first()
+    user = User.query.filter_by(name=name).first()
     if user is None:
         return failure_response("user not found!")
     salt = user.password[:32]
     check_pw = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
     if user.password[32:] != check_pw:
         return failure_response("password incorrect!")
+    return success_response(user.serialize())
 
-
-# 查看好友的lists
-@app.route("/api/<string:uid>/friends_lists/")
-def get_friends_lists(uid):
-    user = User.query.filter_by(id=uid).first()
-    
+@app.route("/api/<int:id>/friends_lists/")
+def get_friends_lists(id):
+    user = User.query.filter_by(id=id).first()
     if user is None:
         return failure_response("user not found!")
-    friends = user.friends
-    return success_response([lst.serialize() for f in friends for lst in f.public_lists if lst.is_public])
+    # friends = user.friends
+    return success_response([f.serialize() for f in user.friends_association])
+    # return success_response([lst.serialize() for f in friends for lst in f.public_lists if lst.is_public])
 
-
-# 查看自己的lists
 @app.route("/api/<int:id>/lists/")
 def get_lists(id):
     user = User.query.filter_by(id=id).first()
     if user is None:
         return failure_response("user not found!")
-    return success_response([c.serialize() for c in User.public_lists.query.all()])
+    return success_response([c.serialize() for c in user.public_lists if c.is_public])
 
-
-@app.route("/api/<string:uid>/lists/<int:list_id>")
-def get_list_by_id(uid, list_id):
-    user = User.query.filter_by(id=uid).first()
+@app.route("/api/<int:id>/lists/<int:list_id>/")
+def get_list_by_id(id, list_id):
+    user = User.query.filter_by(id=id).first()
     if user is None:
         return failure_response("user not found!")
-    public_list = User.public_lists.query.filter_by(public_list_id=list_id).first()
+    public_list = user.public_lists.filter_by(public_list_id=list_id).first()
+    if public_list is None:
+        return failure_response("list not found!")
     return success_response(public_list.serialize())
 
-@app.route("/api/<int:userid>/lists/", methods=['POST'])
-def create_list(userid):
+@app.route("/api/<int:id>/lists/", methods=['POST'])
+def create_list(id):
     body = json.loads(request.data.decode())
-    list_name = body.get('listname')
+    list_name = body.get('list_name')
     is_public = body.get('is_public')
     if is_public is None or list_name is None:
         return failure_response("Please provide access information/name of list")
-    user = User.query.filter_by(id=userid).first()
+    user = User.query.filter_by(id=id).first()
     if user is None:
         return failure_response("user not found!")
-    new_list = PublicList(listname=list_name, publisher_id=userid)
+    new_list = PublicList(list_name=list_name, publisher_id=id)
     db.session.add(new_list)
-    db.session.commit()
-    association = User_PublicList_Association(is_public=is_public)
+    # db.session.commit()
+    association = User_PublicList_Association(user_id=id, is_public=is_public)
     association.public_list = new_list
     user.public_lists.append(association)
     db.session.add(association)
     db.session.commit()
+    return success_response(new_list.serialize())
 
-@app.route("/api/<string:uid>/lists/<int:list_id>/events/", methods=["POST"])
-def create_event(uid, list_id):
+@app.route("/api/<int:id>/lists/<int:list_id>/events/", methods=["POST"])
+def create_event(id, list_id):
     public_list = PublicList.query.filter_by(id=list_id).first()
     if public_list is None:
         return failure_response('list not found!')
@@ -135,29 +128,29 @@ def create_event(uid, list_id):
     db.session.commit()
     return success_response(new_event.serialize(), 201)
 
-
-
-@app.route("/api/<string:uid>/lists/<int:list_id>/<int:event_id>")
-def get_event_by_id(uid, list_id, event_id):
-    user = User.query.filter_by(id=uid).first()
+@app.route("/api/<int:id>/lists/<int:list_id>/events/<int:event_id>/")
+def get_event_by_id(id, list_id, event_id):
+    user = User.query.filter_by(id=id).first()
     if user is None: 
         return failure_response("user not found!")
-    event_list = user.public_lists.query.filter_by(public_list_id = list_id).first()
+    event_list = user.public_lists.filter_by(public_list_id = list_id).first()
     if event_list is None:
         return failure_response("list not found!")
+    event_list = event_list.public_list
     event = event_list.events.filter_by(id = event_id).first()
     if event is None:
             return failure_response("list not found!")
     return success_response(event.serialize())
 
-@app.route("/api/<string:uid>/lists/<int:list_id>/<int:event_id>", methods=["POST"])
-def edit_event_by_id(uid, list_id, event_id):
-    user = User.query.filter_by(id=uid).first()
+@app.route("/api/<int:id>/lists/<int:list_id>/events/<int:event_id>/", methods=["POST"])
+def edit_event_by_id(id, list_id, event_id):
+    user = User.query.filter_by(id=id).first()
     if user is None: 
         return failure_response("user not found!")
-    event_list = user.public_lists.query.filter_by(public_list_id = list_id).first()
+    event_list = user.public_lists.filter_by(public_list_id = list_id).first()
     if event_list is None:
         return failure_response("list not found!")
+    event_list = event_list.public_list
     event = event_list.events.filter_by(id = event_id).first()
     if event is None:
             return failure_response("list not found!")
@@ -165,54 +158,62 @@ def edit_event_by_id(uid, list_id, event_id):
     event.company = body.get('company', event.company)
     event.position = body.get('position', event.position)
     event.reminder = body.get('reminder', event.reminder)
-    db.sesssion.commit()
+    db.session.commit()
+    return success_response(event.serialize())
 
-
-# 添加好友
-@app.route("/api/<string:uid>/friends/add/", methods=['POST'])
-def add_friends(uid):
-    body = json.loads(request.data.decode())
-    user = User.query.filter_by(uid=uid).first()
+@app.route("/api/<int:id>/friends/add/", methods=['POST'])
+def add_friend(id):
+    user = User.query.filter_by(id=id).first()
     if user is None:
         return failure_response("user not found!")
-    search_uid = body.get('uid')
-    if search_uid is None:
-        return failure_response("no uid entered")
-    search_user = User.query.filter_by(uid=search_uid).first()
+    body = json.loads(request.data.decode())
+    search_id = body.get('id')
+    if search_id is None:
+        return failure_response("no id entered")
+    search_user = User.query.filter_by(id=search_id).first()
     if search_user is None:
         return failure_response("user not found!")
+    already_friend = user.friends_association.filter_by(id=search_id).first()
+    if already_friend is not None:
+        return failure_response("you already added this user!")
     search_user.applying_friends.append(user)
-    db.session.add(user)
     db.session.commit()
     return success_response("successfully applied!")
 
-
-# 同意好友申请
-@app.route("/api/<string:uid>/friends/new_friends/<int:friend_uid>/consent/")
-def agree_friend_request(uid, friend_uid):
-    user = User.query.filter_by(uid=uid).first()
+@app.route("/api/<int:id>/friends/accept/<int:friend_id>/")
+def accept_friend_request(id, friend_id):
+    user = User.query.filter_by(id=id).first()
     if user is None:
         return failure_response("user not found!")
-    friend = User.query.filter_by(uid=friend_uid).first()
+    friend = User.query.filter_by(id=friend_id).first()
+    if friend is None:
+        return failure_response("friend user not found!")
+    friend = user.applying_friends_association.filter_by(id=friend_id).first()
+    if friend is None:
+        return failure_response("this user does not have a pending request!")
+    if user.friends_association.filter_by(id=friend_id).first() is not None:
+        return failure_response("this request has already been accepted!")
     user.friends.append(friend)
     user.applying_friends.remove(friend)
     db.session.add(friend)
     db.session.commit()
-    return success_response("successfully applied!")
+    return success_response("friend request accepted!")
 
-
-# 拒绝好友申请
-@app.route("/api/<string:uid>/friends/new_friends/<int:friend_uid>/decline/")
-def decline_friend_request(uid, friend_uid):
-    user = User.query.filter_by(uid=uid).first()
+@app.route("/api/<int:id>/friends/reject/<int:friend_id>/")
+def reject_friend_request(id, friend_id):
+    user = User.query.filter_by(id=id).first()
     if user is None:
         return failure_response("user not found!")
-    friend = User.query.filter_by(uid=friend_uid).first()
+    friend = User.query.filter_by(id=friend_id).first()
+    if friend is None:
+        return failure_response("friend user not found!")
+    friend = user.applying_friends_association.filter_by(id=friend_id).first()
+    if friend is None:
+        return failure_response("this user does not have a pending request!")
     user.applying_friends.remove(friend)
     db.session.add(friend)
     db.session.commit()
-    return success_response("successfully declined!")
-
+    return success_response("friend request rejected!")
 
 if __name__ == "__main__":
     port = os.environ.get('PORT', 5000)
--- hack_challenge/db.py
+++ hack_challenge/db.py
@@ -2,9 +2,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
-
-
 # user 2 user
 friends_association = db.Table(
     'friends',
@@ -24,116 +21,58 @@ list_events_association_table = db.Table(
     db.Column('event_id', db.Integer, db.ForeignKey('event.id'))
 )
 
-# Testing association object for many-to-many relationship
-class UserList(Base):
-    __tablename__ = 'UserList'
-    left_id = db.Column(db.Integer, ForeignKey('user.id'), primary_key=True)
-    right_id = db.Column(db.Integer, ForeignKey('list.id'), primary_key=True)
-    extra_data = db.Column(db.Boolean)
-    user = db.relationship('User', back_populates='lists')
-    list = db.relationship('List', back_populate='users')
-
-class UserFriend(Base):
-    __tablename__ = 'UserFriend'
-    left_id = db.Column(db.Integer, ForeignKey('user.id'), primary_key=True)
-    right_id = db.Column(db.Integer, ForeignKey('friend.id'), primary_key=True)
-    extra_data = db.Column(db.String, nullable=False)
-    user = db.relationship('User', back_populates='friends')
-    friend = db.relationship('Friend', back_populates='users')
-
-
-class User(Base):
-    __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), nullable=False)
-    password = db.Column(db.String(128), nullable=False)
-    lists = db.relationship('UserList', back_populates='user')
-    friends = db.relationship('UserFriend', back_populates='user')
-
-    def __init__(self, **kwargs):
-        self.name = kwargs.get('name')
-        self.password = kwargs.get('password')
-    
-    def serialize(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "lists": [l.serialize() for l in self.lists if l.extra_data],
-            friends = [f.serialize() for f in self.friends if f.extra_data == 'accepted']
-        }
-    
-
-class List(Base):
-    __tablename__ = 'list'
-    id = db.Column(db.Integer, primary_key=True)
-    listName = db.Column(db.String, nullable=False)
-    events = db.relationship("ListEvent", cascade="delete")
-    users = db.relationship('UserList', back_populates='list')
-
-class ListEvent(Base):
-    __tablename__ = 'ListEvent'
-    left_id = db.Column(db.Integer, ForeignKey('list.id'), primary_key=True)
-    right_id = db.Column(db.Integer, ForeignKey('event.id'), primary_key=True)
-    event = db.relationship('Event')
-
-class Event(Base): 
-    __tablename__ = 'event'
-    id = db.Column(db.Integer, primary_key=True)
-    company = db.Column(db.String, nullable=False)
-    position = db.Column(db.String, nullable=False)
-    date = db.Column(db.Datetime, nullable=False)
-
 class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), nullable=False)
-    uid = db.Column(db.String, nullable=False)
     password = db.Column(db.String(128), nullable=False)
-    public_lists = db.relationship("User_PublicList_Association", back_populates="user")
+    public_lists = db.relationship("User_PublicList_Association", back_populates="user", lazy='dynamic')
     
-    # 好友申请
     applying_friends = db.relationship(
         'User',
-        secondary=friends_association,
+        secondary=applying_friends_association,
         primaryjoin=(applying_friends_association.c.user_id1 == id),
         secondaryjoin=(applying_friends_association.c.user_id2 == id),
         lazy='dynamic',
         backref=db.backref('applying_friends_association', lazy='dynamic')
     )
-    # 已有好友
     friends = db.relationship(
         'User',
         secondary=friends_association,
-        primaryjoin=(friends_association.c.user_id1 == id),  # 多对多中）用于从子对象查询其父对象的 condition（child.parents）
-        secondaryjoin=(friends_association.c.user_id2 == id),  # 多对多中）用于从父对象查询其所有子对象的 condition（parent.children）
-        lazy='dynamic',  # 延迟求值，这样才能用 filter_by 等过滤函数
+        primaryjoin=(friends_association.c.user_id1 == id), 
+        secondaryjoin=(friends_association.c.user_id2 == id), 
+        lazy='dynamic',
         backref=db.backref('friends_association', lazy='dynamic')
     )
 
     def __init__(self, **kwargs):
         self.name = kwargs.get('name')
-        self.uid = kwargs.get('uid')
         self.password = kwargs.get('password')
 
-
-
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "friends": [f.serialize() for f in self.friends],
+            "public_list": [pl.serialize() for pl in self.public_lists]
+        }
 
 class PublicList(db.Model):
     __tablename__ = "public_list"
     id = db.Column(db.Integer, primary_key=True)
     publisher_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    listName = db.Column(db.String, nullable=False)
-    events = db.relationship("Event", cascade="delete")
-    users = db.relationship("User_PublicList_Association", back_populates="publiclist")
+    list_name = db.Column(db.String, nullable=False)
+    events = db.relationship("Event", cascade="delete", lazy='dynamic')
+    users = db.relationship("User_PublicList_Association", back_populates="public_list", lazy='dynamic')
     
     def __init__(self, **kwargs):
-        self.listname = kwargs.get('listname')
+        self.list_name = kwargs.get('list_name')
         self.publisher_id = kwargs.get('publisher_id')
 
     def serialize(self):
         return {
             "id": self.id,
-            "listName": self.listName,
+            "list_name": self.list_name,
             "events": [e.serialize() for e in self.events]
         }
 
@@ -147,12 +86,14 @@ class User_PublicList_Association(db.Model):
     public_list = db.relationship("PublicList", back_populates="users")
     
     def __init__(self, **kwargs):
+        self.user_id = kwargs.get('user_id')
         self.is_public = kwargs.get('is_public')
+
     def serialize(self):
         return {
-            "userid": self.user_id,
+            "user_id": self.user_id,
             "public_list_id": self.public_list_id,
-            "public_list": [l.serialize() for l in self.public_list]    
+            "public_list": self.public_list.serialize()
         }
 
 class Event(db.Model):
@@ -162,23 +103,21 @@ class Event(db.Model):
     position = db.Column(db.String, nullable=False)
     reminder = db.Column(db.String, nullable=False)
     public_list_id = db.Column(db.Integer, db.ForeignKey('public_list.id'))
+
     def __init__(self, **kwargs):
         self.company = kwargs.get('company')
         self.position = kwargs.get('position')
         self.reminder = kwargs.get('reminder')
         self.public_list_id=kwargs.get('public_list_id')
+    
     def serialize(self):
         return {
             "id": self.id,
             "company": self.company,
             "position": self.position,
+            "reminder": self.reminder
         }
-
 
 class Image(db.Model):
     __tablename__ = "images"
     id = db.Column(db.Integer, primary_key=True)
-
-
-
-
